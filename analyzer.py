62922edd26b589cc245c8e2dcad746e4928de8cd
Noam <noamsb11.5@gmail.com>
2026-07-14 14:49:18 +0300

    refactor checks and analyzer to use clean helper functions in checks.py

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,5 @@
 
-from checks import is_external, is_sensitive_port
+from checks import is_external, is_sensitive_port, is_large_data, is_abnormal_hour
 
 
 
@@ -9,14 +9,16 @@ def external_ips(all_lines:list) -> list:
 
 
 
-def sensitive_port(all_lines:list) -> list :
+def sensitive_ports(all_lines:list) -> list :
     return [line for line in all_lines if is_sensitive_port(line[3])]
 
-
-def large_data(all_lines:list) -> list:
-    return [line for line in all_lines if int(line[5]) >= 5000 ]
-
+def large_datasets(all_lines: list) -> list:
+    return [line for line in all_lines if is_large_data(line[5])]
 
 def tag_large_data(all_lines:list) :
     return [line + ["NORMAL"] if int(line[5]) < 5000 else line + ["LARGE"] for line in all_lines]
+
+def abnormal_hours(all_lines: list) -> list:
+    return [line for line in all_lines if is_abnormal_hour(line[0])]
+
 
--- checks.py
+++ checks.py
@@ -9,7 +9,13 @@ def is_external(ip: str) -> bool:
 def is_sensitive_port(port:str) -> bool:
     return port in config.SENSITIVE_PORTS
 
+def is_large_data(size: str) -> bool:
+    return int(size) >= config.MAX_NORMAL_SIZE
 
+def is_abnormal_hour(timestamp: str) -> bool:
+    time_part = timestamp.split(" ")[1]
+    hour = int(time_part.split(":")[0])
+    return hour < config.START_WORK_HOUR or hour > config.END_WORK_HOUR
 
 
 
